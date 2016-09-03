2d8398496ccfc5e50df904a344873428996ed666
elexis <elexis1@users.noreply.github.com>
2016-09-03 23:12:42 +0200

    Move follow player logic to new file.

--- binaries/data/mods/public/gui/session/messages.js
+++ binaries/data/mods/public/gui/session/messages.js
@@ -392,39 +392,7 @@ var g_NotificationsTypes =
 	},
 	"playercommand": function(notification, player)
 	{
-		// For observers, focus the camera on units commanded by the selected player
-		if (!g_FollowPlayer || player != g_ViewedPlayer)
-			return;
-
-		let cmd = notification.cmd;
-
-		// Ignore boring animals
-		let entState = cmd.entities && cmd.entities[0] && GetEntityState(cmd.entities[0]);
-		if (entState && entState.identity && entState.identity.classes &&
-				entState.identity.classes.indexOf("Animal") != -1)
-			return;
-
-		// Focus the building to construct
-		if (cmd.type == "repair")
-		{
-			let targetState = GetEntityState(cmd.target);
-			if (targetState)
-				Engine.CameraMoveTo(targetState.position.x, targetState.position.z);
-		}
-		// Focus commanded entities, but don't lose previous focus when training units
-		else if (cmd.type != "train" && cmd.type != "research" && entState)
-			setCameraFollow(cmd.entities[0]);
-
-		// Select units affected by that command
-		let selection = [];
-		if (cmd.entities)
-			selection = cmd.entities;
-		if (cmd.target)
-			selection.push(cmd.target);
-
-		// Allow gaia in selection when gathering
-		g_Selection.reset();
-		g_Selection.addList(selection, false, cmd.type == "gather");
+		focusPlayerCommand(notification.cmd, player);
 	}
 };
 
