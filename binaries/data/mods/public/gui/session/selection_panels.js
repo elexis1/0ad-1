79e0c0bd7ef8fdf1109a196b460342ded2480440
elexis <elexis1@users.noreply.github.com>
2016-09-10 17:30:43 +0200

    Remove all line-wrapping code for bartering buttons introduced in 9c797afd20f2e498ead3cbdef5817f57d3f821a1 and fb611c6d66dd733243057fdb18093958a4222558 Revert to the original scale of the barter icons before fb611c6d66dd733243057fdb18093958a4222558 Improve the position of the barter icons by 4 pixel to prevent overlapping.

--- binaries/data/mods/public/gui/session/selection_panels.js
+++ binaries/data/mods/public/gui/session/selection_panels.js
@@ -89,20 +89,9 @@ g_SelectionPanels.Alert = {
 g_SelectionPanels.Barter = {
 	"getMaxNumberOfItems": function()
 	{
-		let barterPanel = Engine.GetGUIObjectByName("unitBarterPanel");
-		let childCount = barterPanel.children.length / 2;
-		this.rowLength = Math.ceil(childCount / 2);
-
-		if (g_ResourceData.GetCodes().length > this.rowLength)
-		{
-			let panelSize = barterPanel.size;
-			panelSize.top = 3;
-			barterPanel.size = panelSize;
-		}
-
-		return childCount;
+		return 4;
 	},
-	"rowLength": 1,
+	"rowLength": 4,
 	"getItems": function(unitEntState, selection)
 	{
 		if (!unitEntState.barterMarket)
@@ -179,11 +168,8 @@ g_SelectionPanels.Barter = {
 		button.Sell.hidden = false;
 		selectionIcon.hidden = !isSelected;
 
-		// Ensure the buy button is right beneath the sell button of the same resource,
-		// even when using multiple lines.
-		setPanelObjectPosition(button.Sell, data.i + (data.i >= data.rowLength ? data.rowLength : 0), data.rowLength);
-		setPanelObjectPosition(button.Buy, data.i + data.rowLength * (data.i >= data.rowLength ? 2 : 1), data.rowLength);
-
+		setPanelObjectPosition(button.Sell, data.i, data.rowLength);
+		setPanelObjectPosition(button.Buy, data.i + data.rowLength, data.rowLength);
 		return true;
 	}
 };
