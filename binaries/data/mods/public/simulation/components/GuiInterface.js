0ADMods/resource_agnostic

Modify ai common api to de-hardcode game resources
* Also updates the resource json files to carry data that is passed to the resource mapping part of the ai api

simulation/components/GuiInterface.js
@@ -149,6 +149,9 @@ GuiInterface.prototype.GetSimulationState = function(player)
 
 	// Add Resource Codes
 	ret.resources = Resources.GetCodes();
+	ret.aiResourceAnalysis = {};
+	for (let res of ret.resources)
+		ret.aiResourceAnalysis[res] = Resources.GetResource(res).aiAnalysis || null;
 
 	// Add basic statistics to each player
 	var cmpPlayerMan = Engine.QueryInterface(SYSTEM_ENTITY, IID_PlayerManager);

elexis1/0ad-1

binaries/data/mods/public/simulation/components/GuiInterface.js
@@ -154,6 +154,9 @@ GuiInterface.prototype.GetSimulationState = function()
 
 	// Add Resource Codes
 	ret.resources = Resources.GetCodes();
+	ret.aiResourceAnalysis = {};
+	for (let res of ret.resources)
+		ret.aiResourceAnalysis[res] = Resources.GetResource(res).aiAnalysisInfluenceGroup || null;
 
 	// Add basic statistics to each player
 	for (let i = 0; i < numPlayers; ++i)