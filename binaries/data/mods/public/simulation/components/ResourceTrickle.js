0ADMods/resource_agnostic

Modify components to use the new helper script to determine available resource types

simulation/components/ResourceTrickle.js
@@ -2,17 +2,13 @@ function ResourceTrickle() {}
 
 ResourceTrickle.prototype.Schema = 
 	"<a:help>Controls the resource trickle ability of the unit.</a:help>" +
-	"<element name='FoodRate' a:help='Food given to the player every interval'>" +
-		"<ref name='nonNegativeDecimal'/>" +
-	"</element>" +
-	"<element name='WoodRate' a:help='Wood given to the player every interval'>" +
-		"<ref name='nonNegativeDecimal'/>" +
-	"</element>" +
-	"<element name='StoneRate' a:help='Stone given to the player every interval'>" +
-		"<ref name='nonNegativeDecimal'/>" +
-	"</element>" +
-	"<element name='MetalRate' a:help='Metal given to the player every interval'>" +
-		"<ref name='nonNegativeDecimal'/>" +
+	"<element name='Rates' a:help='Trickle Rates'>" +
+		"<oneOrMore>" +
+			"<element a:help='Quantity of a Resource given to the player every interval'>" +
+			    "<anyName/>" +
+				"<ref name='nonNegativeDecimal'/>" +
+			"</element>" +
+		"</oneOrMore>" +
 	"</element>" +
 	"<element name='Interval' a:help='Number of miliseconds must pass for the player to gain the next trickle.'>" +
 		"<ref name='nonNegativeDecimal'/>" +
@@ -33,31 +29,26 @@ ResourceTrickle.prototype.GetTimer = function()
 
 ResourceTrickle.prototype.GetRates = function()
 {
-	var foodrate = +this.template.FoodRate;
-	var woodrate = +this.template.WoodRate;
-	var stonerate = +this.template.StoneRate;
-	var metalrate = +this.template.MetalRate;
-
-	foodrate = ApplyValueModificationsToEntity("ResourceTrickle/FoodRate", foodrate, this.entity);
-	woodrate = ApplyValueModificationsToEntity("ResourceTrickle/WoodRate", woodrate, this.entity);
-	stonerate = ApplyValueModificationsToEntity("ResourceTrickle/StoneRate", stonerate, this.entity);
-	metalrate = ApplyValueModificationsToEntity("ResourceTrickle/MetalRate", metalrate, this.entity);
+	var ret = {};
+	var resCodes = Resources.GetCodes();
+	for (let res in this.template.Rates)
+	{
+		res = res.toLowerCase();
+		if (resCodes.indexOf(res) < 0)
+			continue;
+		
+		ret[res] = ApplyValueModificationsToEntity("ResourceTrickle/Rates/"+res, +this.template.Rates[res], this.entity);
+	}
 	
-	return {"food": foodrate, "wood": woodrate, "stone": stonerate, "metal": metalrate };
+	return ret;
 };
 
 // Do the actual work here
 ResourceTrickle.prototype.Trickle = function(data, lateness)
 {
-	// Get the rates
-	var rates = this.GetRates();
-	
-	// Get the player
 	var cmpPlayer = QueryOwnerInterface(this.entity, IID_Player);
 	if (cmpPlayer)
-		for (var resource in rates)
-			cmpPlayer.AddResource(resource, rates[resource]);
-	
+		cmpPlayer.AddResources(this.GetRates());	
 };
 
 Engine.RegisterComponentType(IID_ResourceTrickle, "ResourceTrickle", ResourceTrickle);

elexis1/0ad-1

binaries/data/mods/public/simulation/components/ResourceTrickle.js
@@ -3,28 +3,12 @@ function ResourceTrickle() {}
 ResourceTrickle.prototype.Schema = 
 	"<a:help>Controls the resource trickle ability of the unit.</a:help>" +
 	"<element name='Rates' a:help='Trickle Rates'>" +
-		"<interleave>" +
-			"<optional>" +
-				"<element name='food' a:help='Food given to the player every interval'>" +
-					"<ref name='nonNegativeDecimal'/>" +
-				"</element>" +
-			"</optional>" +
-			"<optional>" +
-				"<element name='wood' a:help='Wood given to the player every interval'>" +
-					"<ref name='nonNegativeDecimal'/>" +
-				"</element>" +
-			"</optional>" +
-			"<optional>" +
-				"<element name='stone' a:help='Stone given to the player every interval'>" +
-					"<ref name='nonNegativeDecimal'/>" +
-				"</element>" +
-			"</optional>" +
-			"<optional>" +
-				"<element name='metal' a:help='Metal given to the player every interval'>" +
-					"<ref name='nonNegativeDecimal'/>" +
-				"</element>" +
-			"</optional>" +
-		"</interleave>" +
+		"<oneOrMore>" +
+			"<element a:help='Quantity of a Resource given to the player every interval'>" +
+			    "<anyName/>" +
+				"<ref name='nonNegativeDecimal'/>" +
+			"</element>" +
+		"</oneOrMore>" +
 	"</element>" +
 	"<element name='Interval' a:help='Number of miliseconds must pass for the player to gain the next trickle.'>" +
 		"<ref name='nonNegativeDecimal'/>" +
@@ -46,8 +30,13 @@ ResourceTrickle.prototype.GetTimer = function()
 ResourceTrickle.prototype.GetRates = function()
 {
 	var rates = {};
+	var resCodes = Resources.GetCodes();
 	for (var resource in this.template.Rates)
+	{
+		if (resCodes.indexOf(resource) < 0)
+			continue;
 		rates[resource] = ApplyValueModificationsToEntity("ResourceTrickle/Rates/"+resource, +this.template.Rates[resource], this.entity);
+	}
 
 	return rates;
 };
@@ -55,13 +44,9 @@ ResourceTrickle.prototype.GetRates = function()
 // Do the actual work here
 ResourceTrickle.prototype.Trickle = function(data, lateness)
 {
-	var cmpPlayer = QueryOwnerInterface(this.entity);
-	if (!cmpPlayer)
-		return;
-
-	var rates = this.GetRates();
-	for (var resource in rates)
-		cmpPlayer.AddResource(resource, rates[resource]);
+	var cmpPlayer = QueryOwnerInterface(this.entity, IID_Player);
+	if (cmpPlayer)
+		cmpPlayer.AddResources(this.GetRates());
 };
 
 Engine.RegisterComponentType(IID_ResourceTrickle, "ResourceTrickle", ResourceTrickle);