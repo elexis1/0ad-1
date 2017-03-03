ccbbde2309ae52598796ce7273a57d2409f253f8
elexis <elexis1@users.noreply.github.com>
2017-03-03 03:13:11 +0100

    Imaroks ready fix from r19248

--- binaries/data/mods/public/gui/gamesetup/gamesetup.js
+++ binaries/data/mods/public/gui/gamesetup/gamesetup.js
@@ -1044,7 +1044,7 @@ function handleReadyMessage(message)
 
 	if (!g_IsController)
 		return;
-	g_PlayerAssignments[message.guid].status = +message.status == 1;
+	g_PlayerAssignments[message.guid].status = message.status;
 	updateGUIObjects();
 }
 
