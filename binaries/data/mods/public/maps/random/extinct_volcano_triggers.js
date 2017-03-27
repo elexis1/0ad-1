00746b29883d2c1afbc975fad1b3cb152244c95c
elexis <elexis1@users.noreply.github.com>
2017-03-27 15:03:57 +0200

    Animals drown. Trees, mines and other resources are replaced with the identical actor. Performance improvement and selection bugfix by not sending MT_EntityRenamed. Made debugging two variables instead of messing with the balancing. Bit more randomization.

--- binaries/data/mods/public/maps/random/extinct_volcano_triggers.js
+++ binaries/data/mods/public/maps/random/extinct_volcano_triggers.js
@@ -1,21 +1,38 @@
+var debugLog = true;
+
+var debugWaterRise = false;
+
 /**
  * Time in minutes when the players will be notified that the water level will rise.
  */
-var waterRiseNotificationTime = 0*15;
+var waterRiseNotificationTime = [20, 22];
 
 /**
  * Time in minutes when the water level starts to rise.
  * Allow players to build up the economy and military for some time.
  */
-var waterRiseStartTime = 0*randFloat(20 , 25);
+var waterRiseStartTime = [25, 30];
 
 /**
- * Time in milliseconds determining how often to increase the water level.
+ * Time in minutes determining how often to increase the water level.
+ * If the water rises too fast, the hills are of no strategic importance,
+ * building structures would be pointless.
+ *
+ * At height 27, most trees are not gatherable anymore and enemies not reachable.
+ * At height 37 most hills are barely usable.
+ *
+ * At min 30 stuff at the ground level should not be gatherable anymore.
+ * At min 60 CC should be destroyed.
+ *
+ * Notice regular and military docks will raise with the water!
  */
-var waterLevelIncreaseFrequency = 2000;
+var waterIncreaseTime = [2, 3];
 
 /**
  * How much height to increase each step.
+ * Each time the water level is changed, the pathfinder grids have to be recomputed.
+ * Therefore raising the level should occur as rarely as possible, i.e. have the value
+ * as big as possible, but as small as needed to keep it visually authentic.
  */
 var waterLevelIncreaseHeight = 1;
 
@@ -25,45 +42,97 @@ var waterLevelIncreaseHeight = 1;
  */
 var maxWaterLevel = 70;
 
-Trigger.prototype.StartingRaisingWater = function()
+Trigger.prototype.RaisingWaterNotification = function()
 {
 	Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface).AddTimeNotification({
 		"message": markForTranslation("The water keeps rising, we have to evacuate soon!"),
 		"translateMessage": true
 	});
+};
 
-	this.DoAfterDelay((waterRiseStartTime - waterRiseNotificationTime) * 60 * 1000, "RaiseWaterLevelStep", {});
+Trigger.prototype.debugLog = function(txt)
+{
+	if (!debugLog)
+		return;
+
+	let time = Math.round(Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer).GetTime() / 60 / 1000);
+	print("DEBUG [" + time + "] " + txt + "\n");
 };
 
 Trigger.prototype.RaiseWaterLevelStep = function()
 {
+	let time = new Date().getTime();
 	let cmpWaterManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_WaterManager);
 	let newLevel = cmpWaterManager.GetWaterLevel() + waterLevelIncreaseHeight;
-	warn("Raising water level to " + Math.round(newLevel));
-
-	var t2 = new Date().getTime();
 	cmpWaterManager.SetWaterLevel(newLevel);
-	warn("Raising water took " + (new Date().getTime() - t2));
+	this.debugLog("Raising water level to " + Math.round(newLevel) + " took " + (new Date().getTime() - time));
 
-	var t0 = new Date().getTime();
+	time = new Date().getTime();
+	let debugTemplates = {};
+	let cmpTemplateManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_TemplateManager);
 	for (let ent of Engine.QueryInterface(SYSTEM_ENTITY, IID_RangeManager).GetEntitiesByPlayer(0))
 	{
 		let cmpPos = Engine.QueryInterface(ent, IID_Position);
 		if (!cmpPos || !cmpPos.IsInWorld() || cmpPos.GetPosition().y >= newLevel)
 			continue;
 
-		ChangeEntityTemplate(ent, "actor|flora/trees/oak.xml");
-	}
-	warn("Checking gaia ents took " + (new Date().getTime() - t0));
+		let templateName = cmpTemplateManager.GetCurrentTemplateName(ent);
+		if (!templateName)
+			return false;
 
-	var t1 = new Date().getTime();
+		let cmpVisualActor = Engine.QueryInterface(ent, IID_Visual);
+		if (!cmpVisualActor)
+			continue;
+
+		// Animals drown
+		let cmpIdentity = QueryMiragedInterface(ent, IID_Identity);
+		let cmpHealth = QueryMiragedInterface(ent, IID_Health);
+		if (cmpHealth && cmpIdentity.HasClass("Unit"))
+		{
+			cmpHealth.Kill();
+			continue;
+		}
+
+		// Resources become actors
+		// Do not use ChangeEntityTemplate for performance and
+		// because we don't need nor want the effects of MT_EntityRenamed
+		// For example not having the new (unselectable) entity selected
+
+		let pos = cmpPos.GetPosition2D();
+		let height = cmpPos.GetHeightOffset();
+		let rot = cmpPos.GetRotation();
+		let template = cmpTemplateManager.GetTemplate(templateName);
+		let actorTemplate = template.VisualActor.Actor;
+		let seed = cmpVisualActor.GetActorSeed();
+		Engine.DestroyEntity(ent);
+
+		let newEnt = Engine.AddEntity("actor|" + actorTemplate);
+		Engine.QueryInterface(newEnt, IID_Visual).SetActorSeed(seed);
+
+		let cmpNewPos = Engine.QueryInterface(newEnt, IID_Position);
+		cmpNewPos.JumpTo(pos.x, pos.y);
+		cmpNewPos.SetHeightOffset(height);
+		cmpNewPos.SetXZRotation(rot.x, rot.z);
+		cmpNewPos.SetYRotation(rot.y);
+
+		debugTemplates[actorTemplate] = (debugTemplates[actorTemplate] || 0) + 1;
+	}
+
+	this.debugLog("Changing gaia entities to actors took " + (new Date().getTime() - time));
+	this.debugLog("Changed the following entities: " + uneval(debugTemplates));
+
+	// Destroy all player entities below the water
+	// Men only once they are fully submerged (because they don't drown when their ankles get wet)
+	debugTemplates = {};
+	time = new Date().getTime();
 	for (let ent of Engine.QueryInterface(SYSTEM_ENTITY, IID_RangeManager).GetNonGaiaEntities())
 	{
-		//let cmpIdentity = Engine.QueryInterface(ent, IID_Identity);
-		//cmpIdentity && MatchesClassList(cmpIdentity.GetClassesList(), targetClasses);
 		let cmpPos = Engine.QueryInterface(ent, IID_Position);
 		if (!cmpPos || !cmpPos.IsInWorld() || cmpPos.GetPosition().y >= newLevel)
 			continue;
+
+		let templateName = cmpTemplateManager.GetCurrentTemplateName(ent);
+		debugTemplates[templateName] = (debugTemplates[templateName] || 0) + 1;
 
 		let cmpHealth = QueryMiragedInterface(ent, IID_Health);
 		if (cmpHealth)
@@ -72,18 +141,18 @@ Trigger.prototype.RaiseWaterLevelStep = function()
 			Engine.DestroyEntity(ent);
 	}
 
-	warn("Checking player ents took " + (new Date().getTime() - t1));
+	this.debugLog("Checking player entities took " + (new Date().getTime() - time));
+	this.debugLog("Destroyed the following player entities: " + uneval(debugTemplates));
 
 	if (newLevel > maxWaterLevel)
 		return;
 
-	// if all players defeated, stop
-	// maxWaterLevel
-	this.DoAfterDelay(waterLevelIncreaseFrequency, "RaiseWaterLevelStep", {});
+	this.DoAfterDelay((debugWaterRise ? 10 : randFloat(...waterIncreaseTime) * 60) * 1000, "RaiseWaterLevelStep", {});
 };
 
 
 {
 	let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
-	cmpTrigger.DoAfterDelay(waterRiseStartTime * 60 * 1000, "StartingRaisingWater", {});
+	cmpTrigger.DoAfterDelay(randFloat(...waterRiseNotificationTime) * 60 * 1000, "RaisingWaterNotification", {});
+	cmpTrigger.DoAfterDelay(debugWaterRise ? 0 : randFloat(...waterRiseStartTime) * 60 * 1000, "RaiseWaterLevelStep", {});
 }
