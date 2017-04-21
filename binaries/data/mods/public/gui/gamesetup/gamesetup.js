elexis1/0ad-1

commit bce824ea98afb65db25ed3a73afdc42275508f07

Fix another persistmatchsettings bug (because of that variable, the updateGameAttributes exited too early). The pms syndrome will have to be addressed in a separate rewrite.

binaries/data/mods/public/gui/gamesetup/gamesetup.js
@@ -913,7 +913,6 @@ function initGUIObjects()
 	initSPTips();
 
 	loadPersistMatchSettings();
-	updateGUIObjects();
 
 	Engine.GetGUIObjectByName("loadingWindow").hidden = true;
 	Engine.GetGUIObjectByName("setupWindow").hidden = false;
@@ -1317,6 +1316,8 @@ function loadPersistMatchSettings()
 	Engine.SetRankedGame(g_GameAttributes.settings.RatingEnabled);
 
 	supplementDefaults();
+
+	g_IsInGuiUpdate = false;
 	updateGameAttributes();
 }
 

commit 469fa08f2e3e7ced838954ccdcd8d7733d0e19b7

Fix bce824e which broke loading the game if persistmatchsettings were disabled

binaries/data/mods/public/gui/gamesetup/gamesetup.js
@@ -198,7 +198,7 @@ var g_ServerPort;
  * States whether the GUI is currently updated in response to network messages instead of user input
  * and therefore shouldn't send further messages to the network.
  */
-var g_IsInGuiUpdate;
+var g_IsInGuiUpdate = false;
 
 /**
  * Don't supplement defaults while supplementing defaults, nor try to read defaults before they are initialized.
@@ -909,6 +909,7 @@ function initGUIObjects()
 	initSPTips();
 
 	loadPersistMatchSettings();
+	updateGameAttributes();
 
 	Engine.GetGUIObjectByName("loadingWindow").hidden = true;
 	Engine.GetGUIObjectByName("setupWindow").hidden = false;
@@ -1318,7 +1319,6 @@ function loadPersistMatchSettings()
 	supplementDefaults();
 
 	g_IsInGuiUpdate = false;
-	updateGameAttributes();
 }
 
 function savePersistMatchSettings()