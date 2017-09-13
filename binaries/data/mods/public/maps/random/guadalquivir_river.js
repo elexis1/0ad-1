aaea847aecc2d609954bfb06c56ccd18850ec033
elexis <elexis1@users.noreply.github.com>
2017-09-13 18:04:15 +0200

    factor out a summand for consistency

--- binaries/data/mods/public/maps/random/guadalquivir_river.js
+++ binaries/data/mods/public/maps/random/guadalquivir_river.js
@@ -189,7 +189,7 @@ var args = {
 	"constraint": stayClasses(clLand, 0),
 	"waterHeight": -3,
 	"waterWidth": 0.07,
-	"someNumber": -3,
+	"someNumber": -3 + 5,
 	"deviation": 0.005,
 	"fadeDist": 0.025,
 	"waterFunc": (ix, iz, h) => {
@@ -230,9 +230,9 @@ for (let ix = 0; ix < mapSize; ++ix)
 
 			let height = args.waterHeight;
 			if (s1 > 0)
-				height = args.someNumber + 5 * s1 / args.fadeDist;
+				height = args.someNumber + 5 * (s1 - args.fadeDist) / args.fadeDist;
 			else if (s2 < 0)
-				height = args.someNumber + 5 * (- s2) / args.fadeDist;
+				height = args.someNumber + 5 * (- s2 - args.fadeDist) / args.fadeDist;
 
 			let isShallowRange =
 				devCoord2 > 0.3 && devCoord2 < 0.4 ||
