e49190dbd668af83c698992cba769a7c588ad78b
elexis <elexis1@users.noreply.github.com>
2017-09-14 16:26:58 +0200

    MISC JS CLEANUP

--- binaries/data/mods/public/maps/random/rmgen/misc.js
+++ binaries/data/mods/public/maps/random/rmgen/misc.js
@@ -173,9 +173,9 @@ function paintRiver(args)
 				let height = args.waterHeight;
 
 				if (s1 > 0)
-					height = args.someNumber - args.someOtherNumber * (s1 - args.fadeDist) / args.fadeDist;
+					height = args.someNumber - args.someOtherNumber * (s1 / args.fadeDist - 1);
 				else if (s2 < 0)
-					height = args.someNumber - args.someOtherNumber * (-s2 - args.fadeDist) / args.fadeDist;
+					height = args.someNumber - args.someOtherNumber * (-s2 / args.fadeDist - 1);
 
 				setHeight(ix, iz, height);
 
