186279d88b6a14ba450ed6abcda162785cffa9d6
elexis <elexis1@users.noreply.github.com>
2017-09-20 17:02:00 +0200

    Volcanic Lands

--- binaries/data/mods/public/maps/random/volcanic_lands.js
+++ binaries/data/mods/public/maps/random/volcanic_lands.js
@@ -35,73 +35,27 @@ var clRock = createTileClass();
 var clMetal = createTileClass();
 var clBaseResource = createTileClass();
 
-var [playerIDs, playerX, playerZ] = radialPlayerPlacement();
-
-for (var i = 0; i < numPlayers; i++)
-{
-	var id = playerIDs[i];
-	log("Creating base for player " + id + "...");
-
-	var radius = scaleByMapSize(15,25);
-
-	var fx = fractionToTiles(playerX[i]);
-	var fz = fractionToTiles(playerZ[i]);
-	var ix = round(fx);
-	var iz = round(fz);
-	addCivicCenterAreaToClass(ix, iz, clPlayer);
-
-	// create the city patch
-	var cityRadius = radius/3;
-	var placer = new ClumpPlacer(PI*cityRadius*cityRadius, 0.6, 0.3, 10, ix, iz);
-	var painter = new LayeredPainter([tRoadWild, tRoad], [1]);
-	createArea(placer, painter, null);
-
-	placeCivDefaultEntities(fx, fz, id);
-
-	// create metal mine
-	var bbAngle = randFloat(0, TWO_PI);
-	var bbDist = 12;
-	var mAngle = bbAngle;
-	while(abs(mAngle - bbAngle) < PI/3)
-		mAngle = randFloat(0, TWO_PI);
-
-	var mDist = 12;
-	var mX = round(fx + mDist * cos(mAngle));
-	var mZ = round(fz + mDist * sin(mAngle));
-	var group = new SimpleGroup(
-		[new SimpleObject(oMetalLarge, 1,1, 0,0)],
-		true, clBaseResource, mX, mZ
-	);
-	createObjectGroup(group, 0);
-
-	// create stone mines
-	mAngle += randFloat(PI/8, PI/4);
-	mX = round(fx + mDist * cos(mAngle));
-	mZ = round(fz + mDist * sin(mAngle));
-	group = new SimpleGroup(
-		[new SimpleObject(oStoneLarge, 1,1, 0,2)],
-		true, clBaseResource, mX, mZ
-	);
-	createObjectGroup(group, 0);
-
-	// create starting trees
-	var hillSize = PI * radius * radius;
-	var num = floor(hillSize / 60);
-	var tries = 10;
-	for (var x = 0; x < tries; ++x)
-	{
-		var tAngle = randFloat(-PI/3, 4*PI/3);
-		var tDist = randFloat(12, 13);
-		var tX = round(fx + tDist * cos(tAngle));
-		var tZ = round(fz + tDist * sin(tAngle));
-		group = new SimpleGroup(
-			[new SimpleObject(oTree, num, num, 0, 3)],
-			false, clBaseResource, tX, tZ
-		);
-		if (createObjectGroup(group, 0, avoidClasses(clBaseResource, 2)))
-			break;
+placeDefaultPlayerBases({
+	"playerPlacement": radialPlayerPlacement(),
+	"playerTileClass": clPlayer,
+	"baseResourceClass": clBaseResource,
+	"cityPatch": {
+		"innerTerrain": tRoadWild,
+		"outerTerrain": tRoad
+	},
+	// No berries, no chicken
+	"metal": {
+		"template": oMetalLarge
+	},
+	"stone": {
+		"template": oStoneLarge
+	},
+	"trees": {
+		"template": oTree,
+		"areaFactor": 1/60
 	}
-}
+	// No decoratives
+});
 RMS.SetProgress(15);
 
 createVolcano(0.5, 0.5, clHill, tCliff, [tLava1, tLava2, tLava3], true, ELEVATION_SET);
