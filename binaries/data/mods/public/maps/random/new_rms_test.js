8878f2540628c746a04d3d2aaa75d09ca732fa28
elexis <elexis1@users.noreply.github.com>
2017-09-20 13:42:25 +0200

    new rms test

--- binaries/data/mods/public/maps/random/new_rms_test.js
+++ binaries/data/mods/public/maps/random/new_rms_test.js
@@ -2,30 +2,8 @@ RMS.LoadLibrary("rmgen");
 
 InitMap();
 
-var numPlayers = getNumPlayers();
-var mapSize = getMapSize();
-
-var clPlayer = createTileClass();
-var clPath = createTileClass();
-var clHill = createTileClass();
-var clForest = createTileClass();
-var clRock = createTileClass();
-var clFood = createTileClass();
-var clBaseResource = createTileClass();
-
-var [playerIDs, playerX, playerZ] = radialPlayerPlacement(0.39);
-
-for (var i=0; i < numPlayers; i++)
-{
-	var id = playerIDs[i];
-	log("Creating base for player " + id + "...");
-
-	var fx = fractionToTiles(playerX[i]);
-	var fz = fractionToTiles(playerZ[i]);
-	var ix = round(fx);
-	var iz = round(fz);
-
-	placeCivDefaultEntities(fx, fz, id);
-}
+placeDefaultPlayerBases({
+	"playerPlacement": radialPlayerPlacement(0.39)
+});
 
 ExportMap();
