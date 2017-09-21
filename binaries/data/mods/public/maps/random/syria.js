c7cca00e9c6f38ef2fffc3bf70b3a18dde9dcd23
elexis <elexis1@users.noreply.github.com>
2017-09-21 19:21:29 +0200

    Syria

--- binaries/data/mods/public/maps/random/syria.js
+++ binaries/data/mods/public/maps/random/syria.js
@@ -57,92 +57,67 @@ var clGrass = createTileClass();
 
 var [playerIDs, playerX, playerZ] = radialPlayerPlacement();
 
-for (var i = 0; i < numPlayers; i++)
+for (let i = 0; i < numPlayers; ++i)
 {
-	var id = playerIDs[i];
-	log("Creating base for player " + id + "...");
-
-	// scale radius of player area by map size
-	var radius = scaleByMapSize(15,25);
-
-	var fx = fractionToTiles(playerX[i]);
-	var fz = fractionToTiles(playerZ[i]);
-	var ix = round(fx);
-	var iz = round(fz);
-
-	// calculate size based on the radius
-	var size = PI * radius * radius;
+	let ix = Math.round(fractionToTiles(playerX[i]));
+	let iz = Math.round(fractionToTiles(playerZ[i]));
 
 	// create the player area
-	var placer = new ClumpPlacer(size, 0.9, 0.5, 10, ix, iz);
-	createArea(placer, paintClass(clPlayer), null);
+	createArea(
+		new ClumpPlacer(getDefaultPlayerTerritoryArea(), 0.9, 0.5, 10, ix, iz),
+		paintClass(clPlayer),
+		null);
 
 	// create the grass patches
-	var grassRadius = floor(scaleByMapSize(16 ,30));
-	placer = new ChainPlacer(2, floor(scaleByMapSize(5, 12)), floor(scaleByMapSize(25, 60)), 1, ix, iz, 0, [grassRadius]);
-	var painter = new LayeredPainter([tGrassSands, tGrass], [3]);
-	createArea(placer, [painter, paintClass(clGrass)], null);
-
-	// create the city patch
-	var cityRadius = 10;
-	placer = new ClumpPlacer(PI*cityRadius*cityRadius, 0.6, 0.3, 10, ix, iz);
-	var painter = new LayeredPainter([tRoadWild, tRoad], [3]);
-	createArea(placer, painter, null);
-
-	placeCivDefaultEntities(fx, fz, id);
-
-	placeDefaultChicken(fx, fz, clBaseResource);
-
-	// create berry bushes
-	var bbAngle = randFloat(0, TWO_PI);
-	var bbDist = 12;
-	var bbX = round(fx + bbDist * cos(bbAngle));
-	var bbZ = round(fz + bbDist * sin(bbAngle));
-	var group = new SimpleGroup(
-		[new SimpleObject(oBush, 5,5, 0,3)],
-		true, clBaseResource, bbX, bbZ
-	);
-	createObjectGroup(group, 0);
-
-	// create metal mine
-	var mAngle = bbAngle;
-	while(abs(mAngle - bbAngle) < PI/3)
-	{
-		mAngle = randFloat(0, TWO_PI);
-	}
-	var mDist = 11;
-	var mX = round(fx + mDist * cos(mAngle));
-	var mZ = round(fz + mDist * sin(mAngle));
-	group = new SimpleGroup(
-		[new SimpleObject(oMetalLarge, 1,1, 0,0), new RandomObject(aBushes, 2,4, 0,2)],
-		true, clBaseResource, mX, mZ
-	);
-	createObjectGroup(group, 0);
-
-	// create stone mines
-	mAngle += randFloat(PI/8, PI/4);
-	mX = round(fx + mDist * cos(mAngle));
-	mZ = round(fz + mDist * sin(mAngle));
-	group = new SimpleGroup(
-		[new SimpleObject(oStoneLarge, 1,1, 0,2), new RandomObject(aBushes, 2,4, 0,2)],
-		true, clBaseResource, mX, mZ
-	);
-	createObjectGroup(group, 0);
-
-	// create starting trees
-	var num = 3;
-	var tAngle = randFloat(-PI/3, 4*PI/3);
-	var tDist = randFloat(11, 13);
-	var tX = round(fx + tDist * cos(tAngle));
-	var tZ = round(fz + tDist * sin(tAngle));
-	group = new SimpleGroup(
-		[new SimpleObject(pickRandom([oPalm, oTamarix]), num, num, 0,5)],
-		false, clBaseResource, tX, tZ
-	);
-	createObjectGroup(group, 0, avoidClasses(clBaseResource,2));
+	createArea(
+		new ChainPlacer(
+			2,
+			Math.floor(scaleByMapSize(5, 12)),
+			Math.floor(scaleByMapSize(25, 60)),
+			1,
+			ix,
+			iz,
+			0,
+			[Math.floor(scaleByMapSize(16 ,30))]),
+		[
+			new LayeredPainter([tGrassSands, tGrass], [3]),
+			paintClass(clGrass)
+		],
+		null);
 }
-
 RMS.SetProgress(10);
+
+placeDefaultPlayerBases({
+	"playerPlacement": [playerIDs, playerX, playerZ],
+	// playerTileClass marked above
+	"baseResourceClass": clBaseResource,
+	"cityPatch": {
+		"innerTerrain": tRoadWild,
+		"outerTerrain": tRoad,
+		// radius: 10 TODO
+	},
+	"chicken": {
+	},
+	"berries": {
+		"template": oBush
+	},
+	"metal": {
+		"template": oMetalLarge,
+		//"template_2": aBushes // TODO
+	},
+	"stone": {
+		"template": oStoneLarge,
+		//"templaet2": aBushes // TODO
+	},
+	"trees": {
+		"template": pickRandom([oPalm, oTamarix]),
+		"areaFactor": 1/600,
+		"num": 3 //TODO
+	}
+	// No decoratives
+});
+
+RMS.SetProgress(20);
 
 log("Creating bumps...");
 createAreas(
