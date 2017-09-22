f0f7bf1342c0d1cae072b0cf97a39f3d36151eed
elexis <elexis1@users.noreply.github.com>
2017-09-23 00:43:08 +0200

    Bugfix: Missing 3 tiles map border check. Thus don't place ungatherable trees on ambush and allow later retry loops to test whether the initial mines on tiny maps were actually placed.

--- binaries/data/mods/public/maps/random/rmgen/library.js
+++ binaries/data/mods/public/maps/random/rmgen/library.js
@@ -6,7 +6,10 @@ const CELL_SIZE = 4;
 const HEIGHT_UNITS_PER_METRE = 92;
 const MIN_MAP_SIZE = 128;
 const MAX_MAP_SIZE = 512;
+
+// TODO: Shouldn't have to keep in sync with CCmpRangeManager::LosIsOffWorld, CCmpPathfinder::UpdateGrid,TerrainUpdateHelper
 const MAP_BORDER_WIDTH = 3;
+
 const FALLBACK_CIV = "athen";
 /**
  * Constants needed for heightmap_manipulation.js
@@ -226,7 +229,7 @@ function createObjectGroups(placer, player, constraint, amount, retryFactor = 10
 		"placer": placer,
 		"player": player,
 		"constraint": constraint,
-		"halfMapSize": g_Map.size / 2 - 3
+		"halfMapSize": getMapSize() / 2 - MAP_BORDER_WIDTH
 	};
 
 	return retryPlacing(placeFunc, args, retryFactor, amount, false, behaveDeprecated);
@@ -280,7 +283,7 @@ function createSimpleTerrain(terrain)
 
 function placeObject(x, z, type, player, angle)
 {
-	if (g_Map.validT(x, z, MAP_BORDER_WIDTH))
+	if (g_Map.validT(x, z))
 		g_Map.addObject(new Entity(type, player, x, z, angle));
 }
 
