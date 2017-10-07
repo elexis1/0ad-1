bb9b87d1083e28ef234a6b20984980719fb22ce6
elexis <elexis1@users.noreply.github.com>
2017-10-07 04:16:58 +0200

    Move library.js to map.js, rename rest to constants.js

--- binaries/data/mods/public/maps/random/rmgen/map.js
+++ binaries/data/mods/public/maps/random/rmgen/map.js
@@ -405,3 +405,168 @@ function getTerrainTexture(x, y)
 {
 	return g_Map.getTexture(x, y);
 }
+
+/**
+ * Retries the given function with those arguments as often as specified.
+ */
+function retryPlacing(placeFunc, placeArgs, retryFactor, amount, getResult, behaveDeprecated = false)
+{
+	if (behaveDeprecated && !(placeArgs.placer instanceof SimpleGroup || placeArgs.placer instanceof RandomGroup))
+		warn("Deprecated version of createFoo should only be used for SimpleGroup and RandomGroup placers!");
+
+	let maxFail = amount * retryFactor;
+
+	let results = [];
+	let good = 0;
+	let bad = 0;
+
+	while (good < amount && bad <= maxFail)
+	{
+		let result = placeFunc(placeArgs);
+
+		if (result !== undefined || behaveDeprecated)
+		{
+			++good;
+			if (getResult)
+				results.push(result);
+		}
+		else
+			++bad;
+	}
+	return getResult ? results : good;
+}
+
+/**
+ * Helper function for randomly placing areas and groups on the map.
+ */
+function randomizePlacerCoordinates(placer, halfMapSize)
+{
+	if (isCircularMap())
+	{
+		// Polar coordinates
+		// Uniformly distributed on the disk
+		let r = halfMapSize * Math.sqrt(randFloat(0, 1));
+		let theta = randFloat(0, 2 * PI);
+		placer.x = Math.floor(r * Math.cos(theta)) + halfMapSize;
+		placer.z = Math.floor(r * Math.sin(theta)) + halfMapSize;
+	}
+	else
+	{
+		// Rectangular coordinates
+		placer.x = randIntExclusive(0, getMapSize());
+		placer.z = randIntExclusive(0, getMapSize());
+	}
+}
+
+/**
+ * Helper function for randomly placing areas and groups in the given areas.
+ */
+function randomizePlacerCoordinatesFromAreas(placer, areas)
+{
+	let pt = pickRandom(pickRandom(areas).points);
+	placer.x = pt.x;
+	placer.z = pt.z;
+}
+
+// TODO this is a hack to simulate the old behaviour of those functions
+// until all old maps are changed to use the correct version of these functions
+function createObjectGroupsDeprecated(placer, player, constraint, amount, retryFactor = 10)
+{
+	return createObjectGroups(placer, player, constraint, amount, retryFactor, true);
+}
+
+function createObjectGroupsByAreasDeprecated(placer, player, constraint, amount, retryFactor, areas)
+{
+	return createObjectGroupsByAreas(placer, player, constraint, amount, retryFactor, areas, true);
+}
+
+/**
+ * Attempts to place the given number of areas in random places of the map.
+ * Returns actually placed areas.
+ */
+function createAreas(centeredPlacer, painter, constraint, amount, retryFactor = 10, behaveDeprecated = false)
+{
+	let placeFunc = function (args) {
+		randomizePlacerCoordinates(args.placer, args.halfMapSize);
+		return createArea(args.placer, args.painter, args.constraint);
+	};
+
+	let args = {
+		"placer": centeredPlacer,
+		"painter": painter,
+		"constraint": constraint,
+		"halfMapSize": getMapSize() / 2
+	};
+
+	return retryPlacing(placeFunc, args, retryFactor, amount, true, behaveDeprecated);
+}
+
+/**
+ * Attempts to place the given number of areas in random places of the given areas.
+ * Returns actually placed areas.
+ */
+function createAreasInAreas(centeredPlacer, painter, constraint, amount, retryFactor, areas, behaveDeprecated = false)
+{
+	if (!areas.length)
+		return [];
+
+	let placeFunc = function (args) {
+		randomizePlacerCoordinatesFromAreas(args.placer, args.areas);
+		return createArea(args.placer, args.painter, args.constraint);
+	};
+
+	let args = {
+		"placer": centeredPlacer,
+		"painter": painter,
+		"constraint": constraint,
+		"areas": areas,
+		"halfMapSize": getMapSize() / 2
+	};
+
+	return retryPlacing(placeFunc, args, retryFactor, amount, true, behaveDeprecated);
+}
+
+/**
+ * Attempts to place the given number of groups in random places of the map.
+ * Returns the number of actually placed groups.
+ */
+function createObjectGroups(placer, player, constraint, amount, retryFactor = 10, behaveDeprecated = false)
+{
+	let placeFunc = function (args) {
+		randomizePlacerCoordinates(args.placer, args.halfMapSize);
+		return createObjectGroup(args.placer, args.player, args.constraint);
+	};
+
+	let args = {
+		"placer": placer,
+		"player": player,
+		"constraint": constraint,
+		"halfMapSize": getMapSize() / 2 - MAP_BORDER_WIDTH
+	};
+
+	return retryPlacing(placeFunc, args, retryFactor, amount, false, behaveDeprecated);
+}
+
+/**
+ * Attempts to place the given number of groups in random places of the given areas.
+ * Returns the number of actually placed groups.
+ */
+function createObjectGroupsByAreas(placer, player, constraint, amount, retryFactor, areas, behaveDeprecated = false)
+{
+	if (!areas.length)
+		return 0;
+
+	let placeFunc = function (args) {
+		randomizePlacerCoordinatesFromAreas(args.placer, args.areas);
+		return createObjectGroup(args.placer, args.player, args.constraint);
+	};
+
+	let args = {
+		"placer": placer,
+		"player": player,
+		"constraint": constraint,
+		"areas": areas
+	};
+
+	return retryPlacing(placeFunc, args, retryFactor, amount, false, behaveDeprecated);
+}
