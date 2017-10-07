2db758196b370b7bdfd9d877acd6805bd84cd666
elexis <elexis1@users.noreply.github.com>
2017-10-07 03:58:20 +0200

    Move last misc.js to map.js tileclass.js

--- binaries/data/mods/public/maps/random/rmgen/map.js
+++ binaries/data/mods/public/maps/random/rmgen/map.js
@@ -332,6 +332,27 @@ function placeObject(x, z, type, player, angle)
 		g_Map.addObject(new Entity(type, player, x, z, angle));
 }
 
+function modifyTilesBasedOnHeight(minHeight, maxHeight, mode, func)
+{
+	for (let qx = 0; qx < g_Map.size; ++qx)
+		for (let qz = 0; qz < g_Map.size; ++qz)
+		{
+			let height = g_Map.getHeight(qx, qz);
+			if (mode == 0 && height >  minHeight && height < maxHeight ||
+			    mode == 1 && height >= minHeight && height < maxHeight ||
+			    mode == 2 && height >  minHeight && height <= maxHeight ||
+			    mode == 3 && height >= minHeight && height <= maxHeight)
+			func(qx, qz);
+		}
+}
+
+function paintTerrainBasedOnHeight(minHeight, maxHeight, mode, terrain)
+{
+	modifyTilesBasedOnHeight(minHeight, maxHeight, mode, (qx, qz) => {
+		placeTerrain(qx, qz, terrain);
+	});
+}
+
 function initTerrain(terrain)
 {
 	g_Map.initTerrain(createTerrain(terrain));
