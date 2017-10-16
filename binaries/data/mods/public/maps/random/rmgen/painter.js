b41bca1e156304e6d3b0e8da40c913f338cb7915
elexis <elexis1@users.noreply.github.com>
2017-10-16 18:22:28 +0200

    painter, BreadthFirstSearchPainter

--- binaries/data/mods/public/maps/random/rmgen/painter.js
+++ binaries/data/mods/public/maps/random/rmgen/painter.js
@@ -1,162 +1,81 @@
-// Constants for using SmoothElevationPainter
+/**
+ * Absolute height change.
+ */
 const ELEVATION_SET = 0;
+
+/**
+ * Relative height change.
+ */
 const ELEVATION_MODIFY = 1;
 
-/////////////////////////////////////////////////////////////////////////////
-//	ElevationPainter
-//
-//	Class for painting elevation over an area
-//
-//	elevation: Target elevation/height to be painted
-//
-/////////////////////////////////////////////////////////////////////////////
+/**
+ * A Painter
+ *  - modifies an arbitrary feature in a given area, for instance terrain textures or elevation.
+ *  - requires a paint function that only receives an area argument
+ *  - has to pass arguments in the constructor
+ *
+ * The area it paints on is typically determined by a Placer called from createArea. 
+ */
 
+/**
+ * Sets the given height on the given area.
+ */
 function ElevationPainter(elevation)
 {
 	this.elevation = elevation;
-	this.DX = [0, 1, 1, 0];
-	this.DZ = [0, 0, 1, 1];
 }
 
 ElevationPainter.prototype.paint = function(area)
 {
-	var length = area.points.length;
-	var elevation = this.elevation;
-
-	for (var i=0; i < length; i++)
-	{
-		var pt = area.points[i];
-
-		for (var j=0; j < 4; j++)
+	for (let point of area.points)
+		for (let [dx, dz] of [[0, 0], [1, 0], [0, 1], [1, 1]])
 		{
-			if (g_Map.inMapBounds(pt.x + this.DX[j],pt.z + this.DZ[j]))
-				g_Map.height[pt.x + this.DX[j]][pt.z + this.DZ[j]] = elevation;
+			let x = point.x + dx;
+			let z = point.z + dz;
+
+			if (g_Map.inMapBounds(x, z))
+				g_Map.height[x][z] = this.elevation;
 		}
-	}
 };
 
-/////////////////////////////////////////////////////////////////////////////
-//	LayeredPainter
-//
-//	Class for painting multiple layered terrains over an area
-//
-// 	terrainArray: Array of terrain painter objects
-//	widths: Array of widths for each layer
-//
-/////////////////////////////////////////////////////////////////////////////
-
+/**
+ * Paints multiple layered terrain textures over a given area.
+ */
 function LayeredPainter(terrainArray, widths)
 {
-	if (!(terrainArray instanceof Array))
-	{
+	if (!Array.isArray(terrainArray))
 		throw("LayeredPainter: terrains must be an array!");
-	}
 
-	this.terrains = [];
-	for (var i = 0; i < terrainArray.length; ++i)
-	{
-		this.terrains.push(createTerrain(terrainArray[i]));
-	}
-
+	this.terrains = terrainArray.map(terrain => createTerrain(terrain));
 	this.widths = widths;
 }
 
 LayeredPainter.prototype.paint = function(area)
 {
-	var size = getMapSize();
-	var saw = [];
-	var dist = [];
-
-	// init typed arrays
-	for (var i = 0; i < size; ++i)
-	{
-		saw[i] = new Uint8Array(size);		// bool / uint8
-		dist[i] = new Uint16Array(size);	// uint16
-	}
-
-	// Point queue (implemented with array)
-	var pointQ = [];
-
-	// push edge points
-	var pts = area.points;
-	var length = pts.length;
-	var areaID = area.getID();
-
-	for (var i=0; i < length; i++)
-	{
-		var x = pts[i].x;
-		var z = pts[i].z;
-
-		for (var dx=-1; dx <= 1; dx++)
+	breadthFirstSearchPaint(
+		area,
+		0,
+		(x, z) => g_Map.inMapBounds(x, z),
+		(areaID, x, z) => g_Map.area[x][z] == areaID,
+		(point, distance) =>
 		{
-			var nx = x+dx;
-			for (var dz=-1; dz <= 1; dz++)
+			let width = 0;
+			let i = 0;
+
+			for (; i < this.widths.length; ++i)
 			{
-				var nz = z+dz;
-
-				if (g_Map.inMapBounds(nx, nz) && g_Map.area[nx][nz] != areaID && !saw[nx][nz])
-				{
-					saw[nx][nz] = 1;
-					dist[nx][nz] = 0;
-					pointQ.push(new PointXZ(nx, nz));
-				}
-			}
-		}
-	}
-
-	// do BFS inwards to find distances to edge
-	while (pointQ.length)
-	{
-		var pt = pointQ.shift();	// Pop queue
-		var px = pt.x;
-		var pz = pt.z;
-		var d = dist[px][pz];
-
-		// paint if in area
-		if (g_Map.area[px][pz] == areaID)
-		{
-			var w=0;
-			var i=0;
-
-			for (; i < this.widths.length; i++)
-			{
-				w += this.widths[i];
-				if (w >= d)
-				{
+				width += this.widths[i];
+				if (width >= distance)
 					break;
-				}
 			}
-			this.terrains[i].place(px, pz);
-		}
 
-		// enqueue neighbours
-		for (var dx=-1; dx<=1; dx++)
-		{
-			var nx = px+dx;
-			for (var dz=-1; dz<=1; dz++)
-			{
-				var nz = pz+dz;
-
-				if (g_Map.inMapBounds(nx, nz) && g_Map.area[nx][nz] == areaID && !saw[nx][nz])
-				{
-					saw[nx][nz] = 1;
-					dist[nx][nz] = d+1;
-					pointQ.push(new PointXZ(nx, nz));
-				}
-			}
-		}
-	}
+			this.terrains[i].place(point.x, point.z);
+		});
 };
 
-/////////////////////////////////////////////////////////////////////////////
-//	MultiPainter
-//
-//	Class for applying multiple painters over an area
-//
-//	painters: Array of painter objects
-//
-/////////////////////////////////////////////////////////////////////////////
-
+/**
+ * Apply multiple painters over a given area.
+ */
 function MultiPainter(painters)
 {
 	this.painters = painters;
@@ -164,25 +83,17 @@ function MultiPainter(painters)
 
 MultiPainter.prototype.paint = function(area)
 {
-	for (var i=0; i < this.painters.length; i++)
-	{
-		this.painters[i].paint(area);
-	}
+	for (let painter of this.painters)
+		painter.paint(area);
 };
 
-/////////////////////////////////////////////////////////////////////////////
-//	SmoothElevationPainter
-//
-//	Class for painting elevation smoothly over an area
-//
-//	type: Type of elevation modification
-//			ELEVATION_MODIFY = relative
-//			ELEVATION_SET = absolute
-//	elevation: Target elevation/height of area
-//	blendRadius: How steep the elevation change is
-//
-/////////////////////////////////////////////////////////////////////////////
-
+/**
+ * Class for painting elevation smoothly over an area
+ *
+ * @param type - ELEVATION_MODIFY or ELEVATION_SET.
+ * @param elevation - target height.
+ * @param blendRadius - How steep the elevation change is.
+ */
 function SmoothElevationPainter(type, elevation, blendRadius)
 {
 	this.type = type;
@@ -190,189 +101,98 @@ function SmoothElevationPainter(type, elevation, blendRadius)
 	this.blendRadius = blendRadius;
 
 	if (type != ELEVATION_SET && type != ELEVATION_MODIFY)
-	{
-		throw("SmoothElevationPainter: invalid type '"+type+"'");
-	}
+		throw("SmoothElevationPainter: invalid type '" + type + "'");
 }
 
 SmoothElevationPainter.prototype.checkInArea = function(areaID, x, z)
 {
-	// Check given tile and its neighbors
-	return (
-		(g_Map.inMapBounds(x, z) && g_Map.area[x][z] == areaID)
-		|| (g_Map.inMapBounds(x-1, z) && g_Map.area[x-1][z] == areaID)
-		|| (g_Map.inMapBounds(x, z-1) && g_Map.area[x][z-1] == areaID)
-		|| (g_Map.inMapBounds(x-1, z-1) && g_Map.area[x-1][z-1] == areaID)
-	);
+	return g_Map.inMapBounds(x, z) && g_Map.area[x][z] == areaID ||
+	       g_Map.inMapBounds(x-1, z) && g_Map.area[x-1][z] == areaID ||
+	       g_Map.inMapBounds(x, z-1) && g_Map.area[x][z-1] == areaID ||
+	       g_Map.inMapBounds(x-1, z-1) && g_Map.area[x-1][z-1] == areaID;
 };
 
 SmoothElevationPainter.prototype.paint = function(area)
 {
-	var pointQ = [];
-	var pts = area.points;
-	var heightPts = [];
-
-	var mapSize = getMapSize()+1;
-
-	var saw = [];
-	var dist = [];
-	var gotHeightPt = [];
-	var newHeight = [];
-
-	// init typed arrays
-	for (var i = 0; i < mapSize; ++i)
+	let mapSize = getMapSize() + 1;
+	let gotHeightPt = [];
+	let newHeight = [];
+	for (let i = 0; i < mapSize; ++i)
 	{
-		saw[i] = new Uint8Array(mapSize);			// bool / uint8
-		dist[i] = new Uint16Array(mapSize);			// uint16
-		gotHeightPt[i] = new Uint8Array(mapSize);	// bool / uint8
-		newHeight[i] = new Float32Array(mapSize);	// float32
+		gotHeightPt[i] = new Uint8Array(mapSize);
+		newHeight[i] = new Float32Array(mapSize);
 	}
 
-	var length = pts.length;
-	var areaID = area.getID();
-
-	// get a list of all points
-	for (var i=0; i < length; i++)
-	{
-		var x = pts[i].x;
-		var z = pts[i].z;
-
-		for (var dx=-1; dx <= 2; dx++)
+	// Get a list of all points
+	let heightPoints = [];
+	for (let point of area.points)
+		for (let dx = -1; dx <= 2; ++dx)
 		{
-			var nx = x+dx;
-			for (var dz=-1; dz <= 2; dz++)
+			let nx = point.x + dx;
+			for (let dz = -1; dz <= 2; ++dz)
 			{
-				var nz = z+dz;
+				let nz = point.z + dz;
 
 				if (g_Map.validH(nx, nz) && !gotHeightPt[nx][nz])
 				{
 					gotHeightPt[nx][nz] = 1;
-					heightPts.push(new PointXZ(nx, nz));
+					heightPoints.push(new PointXZ(nx, nz));
 					newHeight[nx][nz] = g_Map.height[nx][nz];
 				}
 			}
 		}
-	}
 
-	// push edge points
-	for (var i=0; i < length; i++)
+	breadthFirstSearchPaint(
+		area,
+		1,
+		(x, z) => g_Map.validH(x, z),
+		(areaID, x, z) => this.checkInArea(areaID, x, z),
+		(point, distance) =>
+		{
+			let a = 1;
+			if (distance <= this.blendRadius)
+				a = (distance - 1) / this.blendRadius;
+
+			if (this.type == ELEVATION_SET)
+				newHeight[point.x][point.z] = (1 - a) * g_Map.height[point.x][point.z];
+
+			newHeight[point.x][point.z] += a * this.elevation;
+		});
+
+	// Smooth everything out
+	for (let point of heightPoints)
 	{
-		var x = pts[i].x;
-		var z = pts[i].z;
-		for (var dx=-1; dx <= 2; dx++)
+		if (!this.checkInArea(area.getID(), point.x, point.z))
+			continue;
+
+		let sum = 8 * newHeight[point.x][point.z];
+		let count = 8;
+
+		for (let dx = -1; dx <= 1; ++dx)
 		{
-			var nx = x+dx;
-			for (var dz=-1; dz <= 2; dz++)
+			let nx = point.x + dx;
+
+			for (let dz = -1; dz <= 1; ++dz)
 			{
-				var nz = z+dz;
+				let nz = point.z + dz;
 
-				if (g_Map.validH(nx, nz) && !this.checkInArea(areaID, nx, nz) && !saw[nx][nz])
+				if (g_Map.validH(nx, nz))
 				{
-					saw[nx][nz]= 1;
-					dist[nx][nz] = 0;
-					pointQ.push(new PointXZ(nx, nz));
-				}
-			}
-		}
-	}
-
-	// do BFS inwards to find distances to edge
-	while(pointQ.length)
-	{
-		var pt = pointQ.shift();
-		var px = pt.x;
-		var pz = pt.z;
-		var d = dist[px][pz];
-
-		// paint if in area
-		if (g_Map.validH(px, pz) && this.checkInArea(areaID, px, pz))
-		{
-			if (d <= this.blendRadius)
-			{
-				var a = (d-1) / this.blendRadius;
-				if (this.type == ELEVATION_SET)
-				{
-					newHeight[px][pz] = a*this.elevation + (1-a)*g_Map.height[px][pz];
-				}
-				else
-				{	// type == MODIFY
-					newHeight[px][pz] += a*this.elevation;
-				}
-			}
-			else
-			{	// also happens when blendRadius == 0
-				if (this.type == ELEVATION_SET)
-				{
-					newHeight[px][pz] = this.elevation;
-				}
-				else
-				{	// type == MODIFY
-					newHeight[px][pz] += this.elevation;
+					sum += newHeight[nx][nz];
+					++count;
 				}
 			}
 		}
 
-		// enqueue neighbours
-		for (var dx=-1; dx <= 1; dx++)
-		{
-			var nx = px+dx;
-			for (var dz=-1; dz <= 1; dz++)
-			{
-				var nz = pz+dz;
-
-				if (g_Map.validH(nx, nz) && this.checkInArea(areaID, nx, nz) && !saw[nx][nz])
-				{
-					saw[nx][nz] = 1;
-					dist[nx][nz] = d+1;
-					pointQ.push(new PointXZ(nx, nz));
-				}
-			}
-		}
-	}
-
-	length = heightPts.length;
-
-	// smooth everything out
-	for (var i = 0; i < length; ++i)
-	{
-		var pt = heightPts[i];
-		var px = pt.x;
-		var pz = pt.z;
-
-		if (this.checkInArea(areaID, px, pz))
-		{
-			var sum = 8 * newHeight[px][pz];
-			var count = 8;
-
-			for (var dx=-1; dx <= 1; dx++)
-			{
-				var nx = px+dx;
-				for (var dz=-1; dz <= 1; dz++)
-				{
-					var nz = pz+dz;
-
-					if (g_Map.validH(nx, nz))
-					{
-						sum += newHeight[nx][nz];
-						count++;
-					}
-				}
-			}
-
-			g_Map.height[px][pz] = sum/count;
-		}
+		g_Map.height[point.x][point.z] = sum / count;
 	}
 };
 
-/////////////////////////////////////////////////////////////////////////////
-//	TerrainPainter
-//
-//	Class for painting a terrain over an area
-//
-//	terrain: Terrain placer object
-//
-/////////////////////////////////////////////////////////////////////////////
-
+/**
+ * Paints a given terrain over a given area.
+ *
+ * @param terrain - Terrain texture or texture array. Can contain entities when used with TERRAIN_SEPARATOR.
+ */
 function TerrainPainter(terrain)
 {
 	this.terrain = createTerrain(terrain);
@@ -380,23 +200,13 @@ function TerrainPainter(terrain)
 
 TerrainPainter.prototype.paint = function(area)
 {
-	var length = area.points.length;
-	for (var i=0; i < length; i++)
-	{
-		var pt = area.points[i];
-		this.terrain.place(pt.x, pt.z);
-	}
+	for (let point of area.points)
+		this.terrain.place(point.x, point.z);
 };
 
-/////////////////////////////////////////////////////////////////////////////
-//	TileClassPainter
-//
-//	Class for painting tileClasses over an area
-//
-//	tileClass: TileClass object
-//
-/////////////////////////////////////////////////////////////////////////////
-
+/**
+ * Paints a given tileclass over a given area.
+ */
 function TileClassPainter(tileClass)
 {
 	this.tileClass = tileClass;
@@ -404,23 +214,13 @@ function TileClassPainter(tileClass)
 
 TileClassPainter.prototype.paint = function(area)
 {
-	var length = area.points.length;
-	for (var i=0; i < length; i++)
-	{
-		var pt = area.points[i];
-		this.tileClass.add(pt.x, pt.z);
-	}
+	for (let point of area.points)
+		this.tileClass.add(point.x, point.z);
 };
 
-/////////////////////////////////////////////////////////////////////////////
-//	TileClassUnPainter
-//
-//	Class for unpainting tileClasses over an area
-//
-//	tileClass: TileClass object
-//
-/////////////////////////////////////////////////////////////////////////////
-
+/**
+ * Removes a given tileclass from a given area.
+ */
 function TileClassUnPainter(tileClass)
 {
 	this.tileClass = tileClass;
@@ -428,10 +228,68 @@ function TileClassUnPainter(tileClass)
 
 TileClassUnPainter.prototype.paint = function(area)
 {
-	var length = area.points.length;
-	for (var i=0; i < length; i++)
-	{
-		var pt = area.points[i];
-		this.tileClass.remove(pt.x, pt.z);
-	}
+	for (let point of area.points)
+		this.tileClass.remove(point.x, point.z);
 };
+
+function breadthFirstSearchPaint(area, someDist, validBoundariesFunc, isValidAreaFunc, paintFunc)
+{
+	let mapSize = getMapSize() + someDist; // TODO: what is someDist?
+
+	// Remember which points were visited already and the shortest distance to the area
+	let saw = [];
+	let dist = [];
+	for (let i = 0; i < mapSize; ++i)
+	{
+		saw[i] = new Uint8Array(mapSize);
+		dist[i] = new Uint16Array(mapSize);
+	}
+
+	let pointQueue = [];
+	let areaID = area.getID();
+
+	// Find all points of the area, mark them as seen and set zero distance
+	for (let point of area.points)
+		for (let dx = -1; dx <= 1 + someDist; ++dx)
+		{
+			let nx = point.x + dx;
+			for (let dz = -1; dz <= 1 + someDist; ++dz)
+			{
+				let nz = point.z + dz;
+
+				if (validBoundariesFunc(nx, nz) && !isValidAreaFunc(areaID, nx, nz) && !saw[nx][nz])
+				{
+					saw[nx][nz] = 1;
+					dist[nx][nz] = 0;
+					pointQueue.push(new PointXZ(nx, nz));
+				}
+			}
+		}
+
+	// Visit all direct neighbors of the area, then their neighbors recursively
+	while (pointQueue.length)
+	{
+		let point = pointQueue.shift();
+		let distance = dist[point.x][point.z];
+
+		if (isValidAreaFunc(areaID, point.x, point.z))
+			paintFunc(point, distance);
+
+		// Enqueue neighbours
+		for (let dx = -1; dx <= 1; ++dx)
+		{
+			let nx = point.x + dx;
+			for (let dz = -1; dz <= 1; ++dz)
+			{
+				let nz = point.z + dz;
+
+				if (validBoundariesFunc(nx, nz) && isValidAreaFunc(areaID, nx, nz) && !saw[nx][nz])
+				{
+					saw[nx][nz] = 1;
+					dist[nx][nz] = distance + 1;
+					pointQueue.push(new PointXZ(nx, nz));
+				}
+			}
+		}
+	}
+}
