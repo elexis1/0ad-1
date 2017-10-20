22f7828d67633df089dbe71048ed6fa5c0b5b6f4
elexis <elexis1@users.noreply.github.com>
2017-10-20 03:36:29 +0200

    placer split

--- binaries/data/mods/public/maps/random/rmgen/math.js
+++ binaries/data/mods/public/maps/random/rmgen/math.js
@@ -23,3 +23,18 @@ function rotateCoordinates(x, z, angle, centerX = 0.5, centerZ = 0.5)
 		sin * (x - centerX) + cos * (z - centerZ) + centerZ
 	];
 }
+
+function cubicInterpolation2(coords, v0, v1, v2, v3)
+{
+	let P = (v3 - v2) - (v0 - v1);
+	let Q = (v0 - v1) - P;
+	let R = v2 - v0;
+	let S = v1;
+
+	let interpolate = x => ((P * x + Q) * x + R) * x + S;
+
+	if (Array.isArray(coords))
+		return coords.map(interpolate);
+
+	return interpolate(coords);
+}
