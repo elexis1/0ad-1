aec1b89fd21d379d31ebb5a3bbb9a9c9c8eaeac9
elexis <elexis1@users.noreply.github.com>
2018-08-06 18:54:52 +0200

    Always provide title

--- binaries/data/mods/public/gui/terms/terms.js
+++ binaries/data/mods/public/gui/terms/terms.js
@@ -4,10 +4,8 @@ function init(data)
 {
 	g_TermsPage = data.page;
 
+	Engine.GetGUIObjectByName("title").caption = data.title;
 	Engine.GetGUIObjectByName("mainText").caption = Engine.TranslateLines(Engine.ReadFile("gui/" + data.file + ".txt"));
-
-	if (data.title)
-		Engine.GetGUIObjectByName("title").caption = data.title;
 }
 
 function closeTerms(accepted)
