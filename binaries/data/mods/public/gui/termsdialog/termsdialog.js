b20b88078ce6465972b5edfe3bd295de91591709
elexis <elexis1@users.noreply.github.com>
2018-08-07 13:44:16 +0200

    Language selection completion

--- binaries/data/mods/public/gui/termsdialog/termsdialog.js
+++ binaries/data/mods/public/gui/termsdialog/termsdialog.js
@@ -1,21 +1,51 @@
 var g_TermsPage = "";
+var g_TermsFile = "";
 
 function init(data)
 {
 	g_TermsPage = data.page;
+	g_TermsFile = data.file;
 
 	Engine.GetGUIObjectByName("title").caption = data.title;
 
-	let language = Engine.GetGUIObjectByName("language");
-	language.list = Engine.GetSupportedLocaleDisplayNames();
-	language.list_data = Engine.GetSupportedLocaleDisplayNames();
-
-	selectLanguage(data.file);
+	initLanguageDropdown();
 }
 
-function selectLanguage(file)
+function initLanguageDropdown()
 {
-	Engine.GetGUIObjectByName("mainText").caption = Engine.TranslateLines(Engine.ReadFile("gui/" + file + ".txt"));
+	let displayNames = Engine.GetSupportedLocaleDisplayNames();
+	let baseNames = Engine.GetSupportedLocaleBaseNames();
+
+	let languages = [
+		// en-US
+		{
+			"title": displayNames[0],
+			"localeDict": baseNames[0]
+		}
+	];
+
+	let currentLocaleDict = Engine.GetFallbackToAvailableDictLocale(Engine.GetCurrentLocale());
+	if (currentLocaleDict != languages[0].localeDict)
+		languages.push({
+			"title": displayNames[baseNames.indexOf(currentLocaleDict)] || "error",
+			"localeDict": currentLocaleDict
+		});
+
+	let language = Engine.GetGUIObjectByName("language");
+	languages = prepareForDropdown(languages);
+	language.list = languages.title;
+	language.list_data = languages.localeDict;
+	language.selected = 0;
+}
+
+function selectLanguage()
+{
+	let txt = Engine.ReadFile("gui/" + g_TermsFile + ".txt");
+
+	if (Engine.GetGUIObjectByName("language").selected == 1)
+		txt = Engine.TranslateLines(txt);
+
+	Engine.GetGUIObjectByName("mainText").caption = txt;
 }
 
 function closeTerms(accepted)
