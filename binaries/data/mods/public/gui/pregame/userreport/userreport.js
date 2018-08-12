f680df8ad2b18ab2093c8c8ea6226600a62b1649
elexis <elexis1@users.noreply.github.com>
2018-08-12 22:37:30 +0200

    format userreport status object-oriented style

--- binaries/data/mods/public/gui/pregame/userreport/userreport.js
+++ binaries/data/mods/public/gui/pregame/userreport/userreport.js
@@ -1,3 +1,20 @@
+var g_UserReportStatusFormat = {
+	"disabled": data => translate("disabled"),
+	"proxy": data => translate("connecting to server"),
+	"waiting": data => translate("connecting to server"),
+	"connecting": data => translate("connecting to server"),
+	"sending": data => sprintf(translate("uploading (%f%%)"), Math.floor(100 * data[1])),
+	"completed": data =>
+		data[1] == 200 ?
+			translate("upload succeeded") :
+			sprintf(translate("upload failed (%(errorCode)s)"), {
+				"errorCode": data[2]
+			}),
+	"failed": data => sprintf(translate("upload failed (%(errorMessage)s)"), {
+		"errorMessage": data[1]
+	})
+};
+
 function initUserReport()
 {
 	initTerms({
@@ -6,7 +23,7 @@ function initUserReport()
 			"instruction": translate("Please read and accept the Disclaimer"),
 			"file": "pregame/userreport/Disclaimer",
 			"config": "userreport.disclaimer",
-			"callback": (data) => {
+			"callback": data => {
 				setUserReportEnabled(data.accepted);
 				updateUserReportTermsFeedback();
 			},
@@ -42,41 +59,15 @@ function setUserReportEnabled(enabled)
 
 function updateUserReporterStatus()
 {
+	let statusData = Engine.GetUserReportStatus().split(/:/, 3);
+
 	Engine.GetGUIObjectByName("userReportText").caption =
 		Engine.IsUserReportEnabled() ?
 			'[font="sans-bold-16"]' + translate("Thank you for helping improve 0 A.D.!") + "[/font]\n\n" +
 			translate("Feedback is currently enabled.") + "\n" +
 			sprintf(translate("Status: %(status)s."), {
-				"status": formatUserReportStatus()
+				"status": g_UserReportStatusFormat[statusData[0]] ? g_UserReportStatusFormat[statusData[0]](statusData) : translate("unknown")
 			}) :
 			'[font="sans-bold-16"]' + translate("Help improve 0 A.D.!") + "[/font]\n\n" +
 			translate("You can automatically send us feedback that can help us fix bugs, and improve performance and compatibility.");
-}
-
-function formatUserReportStatus()
-{
-	let d = Engine.GetUserReportStatus().split(/:/, 3);
-
-	if (d[0] == "disabled")
-		return translate("disabled");
-
-	if (d[0] == "connecting")
-		return translate("connecting to server");
-
-	if (d[0] == "sending")
-		return sprintf(translate("uploading (%f%%)"), Math.floor(100 * d[1]));
-
-	if (d[0] == "completed")
-	{
-		let httpCode = d[1];
-		if (httpCode == 200)
-			return translate("upload succeeded");
-
-		return sprintf(translate("upload failed (%(errorCode)s)"), { "errorCode": httpCode });
-	}
-
-	if (d[0] == "failed")
-		return sprintf(translate("upload failed (%(errorMessage)s)"), { "errorMessage": d[2] });
-
-	return translate("unknown");
 }
