ba8dddd380de9b618823ae4b85607404dd313a6a
elexis <elexis1@users.noreply.github.com>
2018-08-13 19:16:30 +0200

    Privacy Policy diff from D1590

--- binaries/data/mods/public/gui/prelobby/common/terms/termslobby.js
+++ binaries/data/mods/public/gui/prelobby/common/terms/termslobby.js
@@ -1,6 +1,8 @@
+var g_TermsButtonHeight = 40;
+
 function initLobbyTerms()
 {
-	initTerms({
+	let terms = {
 		"Service": {
 			"title": translate("Terms of Service"),
 			"instruction": translate("Please read and accept the Terms of Service."),
@@ -18,6 +20,32 @@ function initLobbyTerms()
 			"salt": () => Engine.GetGUIObjectByName("username").caption,
 			"accepted": false,
 			"callback": updateFeedback
+		},
+		"Privacy": {
+			"title": translate("Privacy Policy"),
+			"instruction": translate("Please read and accept the Privacy Policy."),
+			"file": "prelobby/common/terms/Privacy_Policy",
+			"config": "lobby.privacy_policy",
+			"accepted": false,
+			"callback": updateFeedback
 		}
+	};
+
+	Object.keys(terms).forEach((page, i) => {
+
+		let button = Engine.GetGUIObjectByName("termsButton[" + i + "]");
+
+		button.caption = terms[page].title;
+
+		button.onPress = () => {
+			openTerms(page);
+		};
+
+		let size = button.size;
+		size.top = i * g_TermsButtonHeight;
+		size.bottom = i * g_TermsButtonHeight + 28;
+		button.size = size;
 	});
+
+	initTerms(terms);
 }
