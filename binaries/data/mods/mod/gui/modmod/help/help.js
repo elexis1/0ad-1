e9b7693b71ea18a43ba49e2fa3b3c21084c210be
elexis <elexis1@users.noreply.github.com>
2018-09-14 13:11:56 +0200

    Remove mod.io references from mod selector page, except for the download button

--- binaries/data/mods/mod/gui/modmod/help/help.js
+++ binaries/data/mods/mod/gui/modmod/help/help.js
@@ -1,4 +1,16 @@
-function init(data)
+function init()
 {
 	Engine.GetGUIObjectByName("mainText").caption = Engine.TranslateLines(Engine.ReadFile("gui/modmod/help/help.txt"));
+
+	let moddingGuideButton = Engine.GetGUIObjectByName("moddingGuideButton");
+	moddingGuideButton.caption = translate("Modding Guide");
+	moddingGuideButton.onPress = () => {
+		Engine.OpenURL("https://trac.wildfiregames.com/wiki/Modding_Guide");
+	};
+
+	let closeButton = Engine.GetGUIObjectByName("closeButton");
+	closeButton.caption = translate("Close");
+	closeButton.onPress = () => {
+		Engine.PopGuiPage();
+	};
 }
