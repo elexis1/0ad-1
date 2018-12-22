4a68a83b3dc1be53c233cea9b4a8f9e905afd0ee
elexis <elexis1987@gmx.de>
2018-12-22 10:46:59 +0100

    JS OOP rewrite

--- binaries/data/mods/public/gui/networkreport/networkreport.js
+++ binaries/data/mods/public/gui/networkreport/networkreport.js
@@ -15,6 +15,56 @@ var g_ClientListLastUpdate = 0;
 
 var g_SelectedClientGUID;
 
+var g_ClientListEntry = (guid, clientPerformance) => ({
+	"name":
+		setStringTags(g_PlayerAssignments[guid].name, {
+			"color":
+				g_GameAttributes.settings.PlayerData[g_PlayerAssignments[guid].player - 1] ?
+					rgbToGuiColor(g_GameAttributes.settings.PlayerData[g_PlayerAssignments[guid].player - 1].Color) :
+					"white"
+	}),
+	"status":
+		getNetworkWarningText(
+			getNetworkWarningString(guid == Engine.GetPlayerGUID(), clientPerformance),
+			clientPerformance,
+			g_PlayerAssignments[guid].name) || translate("Ok"),
+	"meanRTT": (() => {
+		let lastReceivedTime = clientPerformance.lastReceivedTime > 3000 ? clientPerformance.lastReceivedTime : 0;
+		let meanRTT = Math.max(clientPerformance.meanRTT, lastReceivedTime);
+		return meanRTT == 0 ?
+			translateWithContext("unknown mean roundtriptime", "?") :
+			coloredText(
+				sprintf(translateWithContext("network latency", "%(milliseconds)sms"), {
+					"milliseconds": meanRTT
+				}),
+				efficiencyToColor(1 - meanRTT / inefficientRTT));
+	})(),
+	"packetLoss":
+		// TODO: 0 can also mean perfect connection, for example localhost.
+		clientPerformance.packetLoss == 0 ?
+			translateWithContext("unknown packet loss ratio", "?") :
+			coloredText(
+				sprintf(translateWithContext("network packet loss", "%(packetLossRatio)s%%"), {
+					"packetLossRatio": (clientPerformance.packetLoss * 100).toFixed(1)
+				}),
+				efficiencyToColor(1 - clientPerformance.packetLoss / inefficientPacketLoss))
+});
+
+var g_ClientListOrder = {
+	"name": (guid1, guid2, clientPerformance) =>
+		g_PlayerAssignments[guid1].name.localeCompare(
+		g_PlayerAssignments[guid2].name),
+	"status": (guid1, guid2, clientPerformance) =>
+		getNetworkWarningString(guid1 == Engine.GetPlayerGUID(), clientPerformance[guid1]).localeCompare(
+		getNetworkWarningString(guid2 == Engine.GetPlayerGUID(), clientPerformance[guid2])),
+	"meanRTT": (guid1, guid2, clientPerformance) =>
+		clientPerformance[guid1].meanRTT -
+		clientPerformance[guid2].meanRTT,
+	"packetLoss": (guid1, guid2, clientPerformance) =>
+		clientPerformance[guid1].packetLoss -
+		clientPerformance[guid2].packetLoss
+};
+
 var g_GUIProperties = {
 	"clientList": {
 		"onSelectionColumnChange": () => function() {
@@ -111,50 +161,17 @@ function updateClientList()
 		return;
 
 	let clientList = Engine.GetGUIObjectByName("clientList");
+
 	let guids = Object.keys(clientPerformance).filter(guid => !!g_PlayerAssignments[guid]).sort((guid1, guid2) =>
-		clientList.selected_column_order *
-		(clientList.selected_column == "name" ?
-			g_PlayerAssignments[guid1].name.localeCompare(g_PlayerAssignments[guid2].name) :
-		clientList.selected_column == "status" ?
-			// TODO
-			g_PlayerAssignments[guid1].name.localeCompare(g_PlayerAssignments[guid2].name) :
-			clientPerformance[guid1][clientList.selected_column] - clientPerformance[guid2][clientList.selected_column]));
+		clientList.selected_column_order * g_ClientListOrder[clientList.selected_column](guid1, guid2, clientPerformance));
 
-	clientList.list_name = guids.map(guid => setStringTags(g_PlayerAssignments[guid].name, {
-		"color": g_GameAttributes.settings.PlayerData[g_PlayerAssignments[guid].player - 1] ?
-				rgbToGuiColor(g_GameAttributes.settings.PlayerData[g_PlayerAssignments[guid].player - 1].Color) :
-		        "white"
-	}));
+	let clientListEntries = prepareForDropdown(guids.map(guid => g_ClientListEntry(guid, clientPerformance[guid])));
 
-	clientList.list_status = guids.map(guid =>
-		getNetworkWarningText(
-			getNetworkWarningString(guid == Engine.GetPlayerGUID(), clientPerformance[guid]),
-			clientPerformance[guid],
-			g_PlayerAssignments[guid].name) ||
-		translate("Ok"));
-
-	clientList.list_meanRTT = guids.map(guid =>	{
-		let lastReceivedTime = clientPerformance[guid].lastReceivedTime > 3000 ? clientPerformance[guid].lastReceivedTime : 0;
-		let meanRTT = Math.max(clientPerformance[guid].meanRTT, lastReceivedTime);
-		return meanRTT == 0 ?
-			translateWithContext("unknown mean roundtriptime", "?") :
-			coloredText(
-				sprintf(translateWithContext("network latency", "%(milliseconds)sms"), {
-					"milliseconds": meanRTT
-				}),
-				efficiencyToColor(1 - meanRTT / inefficientRTT))
-	});
-
-	clientList.list_packetLoss = guids.map(guid =>
-		clientPerformance[guid].packetLoss == 0 ?
-			translateWithContext("unknown packet loss ratio", "?") :
-			coloredText(
-				sprintf(translateWithContext("network packet loss", "%(packetLossRatio)s%%"), {
-					"packetLossRatio": (clientPerformance[guid].packetLoss * 100).toFixed(1)
-				}),
-				efficiencyToColor(1 - clientPerformance[guid].packetLoss / inefficientPacketLoss)));
-
+	// TODO: It would be nicer and safer to exchange the entire table at a time
+	for (let column in clientListEntries)
+		//if (("list_" + column) in clientList) TODO: this shouldn't make it crash
+		if (column != "Default")
+			clientList["list_" + column] = clientListEntries[column];
 	clientList.list = guids;
-
 	clientList.selected = clientList.list.indexOf(g_SelectedClientGUID);
 }
